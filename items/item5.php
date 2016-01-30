11983a1c95cd3ef3567159d2d100f245b776ed6f
Aashish Satyajith <ankarathaashish@gmail.com>
2016-01-30 22:59:53 +0530

    updated opportunities

--- items/item5.php
+++ items/item5.php
@@ -7,14 +7,17 @@
           <div class="row">
             <div class="col-md-6">
               <ul>
-                <li>The DAAD WISE internship at Germany is a prestigious one. Applications open till the end of October.</li>
-                <li>The USC's Viterbi programme is a great opportunity as well.</li>
-
+                <li>Charpak Research Internship Program is a great opportunity to work as an intern at a research laboratory in France! Applications accepted till March 13<sup>th</sup>.</li>
+                <li>Caltech's coveted SURF Program has its applications due on February 22<sup>nd</sup>.</li>
+                <li>IIT Madras Summer Fellowship Program has its deadline on 29<sup>th</sup> February.</li>
+                <li>Also, be sure to keep an eye on the Google Summer of Code timeline. The program is currently accepting applications for mentoring organizations, and will start accepting applications from students on March 14<sup>th</sup>.</li>
               </ul>
             </div>
             <div class="col-md-6">
               <ul>
-                <li>Apart from internships abroad, one can also consider various research internships like IAS SRFP,   or at institutes like IITs, IISc etc.</li>
+                <li>Ekalavya, IIT Bombay's signature internship, has also opened call for applications. Apply soon though, as deadline is on February 15<sup>th</sup></li>
+                <li>Visiting Summer Student Program offered by IMSc also has its deadline on February 15<sup>th</sup>.</li>
+                <li>IIT Guwahati is also hosting a Summer Internship Program. This one has its deadline late in March.</li>
                 <li>Also, don't miss out on some amazing learning opportunities at a few startups.</li>
               </ul>
             </div>
