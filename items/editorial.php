70dbf540079bea226f2d5c8e5e43390dcf0fb212
Anas Ansari <anasanzari@gmail.com>
2016-02-07 23:04:03 +0530

    editorial + team page + minor changes

---
+++ items/editorial.php
@@ -0,0 +1,32 @@
+<div class="bb-item" id="item12">
+  <div class="content">
+    <div class="scroller">
+      <div class="light">
+        <h2>The Team</h2>
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-6">
+              <p>“Sometimes it is the people who no one imagines anything of who do the things that no one can imagine.” </p>
+              <p class="author">- <em>Alan Turing</em></p>
+        <p>Every mathematician or a computer scientist would have dreamt of reaching great heights as far as those scaled by Mr. Alan Turing.
+        This man, a brilliant mathematician, logistician and cryptanalyst, needs no further introduction.
+        His contribution to the field of theoretical computer science shaped this field as we see it today.
+        The first issue of the official newsletter of the Department of Computer Science and Engineering,
+        NIT Calicut pays homage to him. The machine, named after its inventor, is a simple yet a highly powerful mathematical model
+         used to simulate the logic of any algorithm.</p>
+            </div>
+            <div class="col-md-6">
+
+
+
+            </div>
+
+
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+</div>
---
+++ items/item11.php
@@ -0,0 +1,47 @@
+<div class="bb-item" id="item11">
+  <div class="content">
+    <div class="scroller">
+      <div class="light">
+        <h2>The Team</h2>
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-3 col-md-offset-3">
+              <h4 class="team">Staff editor</h4 class="team">
+              <ul class="special">
+                <li>Vinod Pathari</li>
+              </ul>
+              </div>
+            <div class="col-md-6">
+
+              <h4 class="team">Student editors</h4 class="team">
+              <ul class="special">
+                <li>Aishwarya Maruvada</li>
+                <li>Aashish Satyajith</li>
+                <li>Piyush Bhopalka</li>
+                <li>Aparna Jayaraj</li>
+              </ul>
+
+            </div>
+            <div class="row">
+              <div class="col-md-12">
+                <div class="socialbuttons">
+                  <a class="social" href="https://www.facebook.com/CSEA.NITC">
+                    <img src="./images/social/facebook.png" />
+                  </a>
+
+                  <a class="social" href="http://assoc.cse.nitc.ac.in">
+                    <img src="./images/social/web.png" />
+                  </a>
+                </div>
+                <p class="center">We are now accepting articles for our next issues. Please drop us a  mail at csed.newsletter@gmail.com</p>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+</div>
