25ba8e0138acd23a97f1566e9215bf6dd4d990a6
simsarulhaqv <simsarulhaqv@gmail.com>
2016-02-23 00:51:30 +0530

    Correct the FOSSMeet date and update speaker info

--- items/item8.php
+++ items/item8.php
@@ -8,7 +8,7 @@
           <div class="row">
             <div class="col-md-6">
               <div class="col-md-8 col-md-offset-2">
-                <h3>FOSSMeet coming up this Feb 27-28! See you all there</h3>
+                <h3>FOSSMeet coming up this Feb 26-28<br />See you all there!</h3>
                 <a href="http://www.fossmeet.in"><img src="./images/fossmeetPoster.jpg" class="img-responsive" /></a>
                 <h4>Register at: <a href="http://www.fossmeet.in">http://www.fossmeet.in</a></h4>
               </div>
@@ -18,16 +18,17 @@
               <ul>
                   <h3>Talks/ Workshops during FOSSMeet 2016</h3>
                   <hr>
-                  <h4>Steven Deobald - The Worst Programmer I ever met</h4>
+		  <br />
+                  <h4>The Worst Programmer I ever met - Steven Deobald</h4>
                   <p>
 Relationships are hard. Software is hard. FOSS communities often represent the most difficult intersection of these two axes.
 Attendees will come out with practical lessons and tools for working in teams, bettering themselves, and avoiding some of software’s most expensive mistakes.
                   </p><br>
-                  <h4>Baiju M - Golang Workshop</h4>
+                  <h4>Golang Workshop - Baiju M</h4>
                   <p>
                       This workshop intent to teach the fundamentals of Go programming language. The workshop will include setting up environment for Go development, basic syntax, concepts and solving few problems.
                   </p><br>
-                  <h4>Abhas Abhinav - Building your own Hackable Keyboard</h4>
+                  <h4>Building your own Hackable Keyboard - Abhas Abhinav</h4>
                   <p>
                       We all use keyboards. And not all of us care enough about them to ensure that, like our operating systems, they are hackable too. What could be better than to
                       build your own keyboard using open hardware designs and components and then program it using free / open source software?
