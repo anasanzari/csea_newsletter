aadcf7cf812e9a0ef7cac7cab7b21a818abe1b43
Piyush Bhopalka <pbhopalka@gmail.com>
2016-02-25 00:47:28 +0530

    Update Gadget gizmo section

--- items/item7.php
+++ items/item7.php
@@ -8,17 +8,25 @@
         <div class="container-fluid">
           <div class="row">
             <div class="col-md-6">
-              <p>The <strong>Asus Chromebit</strong> is a HDMI dongle, which can be connected to a display (TV/ Monitor) to bring Chrome OS functionality to them. Sometime back, Google had unveiled Chromecast, a HDMI dongle which was basically extending a second screen from your phone, tablet or your Chromebook onto the big screen. However, this time with the partnership with Asus, Google has released a full OS on the pendrive-like device. Just plug it in the HDTV and start using. It has been available in India since January for Rs 7,999. </p>
-              <p><strong>Asus Chromebit specifications and features: </strong></p>
-              <p>On the hardware front, it is powered by a Rockchip 3288 ARM-based processor paired with 2GB of RAM and 16GB of onboard storage. The Chromebit also includes connectivity options such as Wi-Fi 802.11ac, Bluetooth 4.0 and a USB 2.0 port.
-                The USB 2.0 port on the dongle lets you connect peripherals such as a USB mouse or keyboard, a pen drive or an external hard disk.</p>
+              <p>The <strong>Asus Chromebit</strong> is a HDMI dongle, which can be connected to a display (TV/ Monitor) to bring Chrome OS functionality to them. Sometime back, Google had unveiled Chromecast, a HDMI dongle which was basically extending a second screen from your phone, tablet or your Chromebook onto the big screen. However, this time in partnership with Asus, Google has released a full OS on a pendrive-like device.
+                  Imagine the time when an OS used to come in CDs and after spending hours on installation, we would finally end up with a system that could do tasks that even feature phones can do today with ease.
+                  Recent advancements have made it possible to simply plug and play an OS and Chromebit is a perfect example for it. It has been available in India since January for a MRP of Rs 7,999<sup>[1]</sup>. </p>
+
+            <p>Chromebit is a powerful device and an innovative product. Using a light-weight OS like Chrome OS, Asus and Google have taken a right decision
+                to turn a normal TV/monitor without the need for an external CPU into a full-fledged computer.</p>
 
             </div>
             <div class="col-md-6">
+                <p><strong>Asus Chromebit specifications and features: </strong></p>
+                <p>On the hardware front, it is powered by a Rockchip 3288 ARM-based processor paired with 2GB of RAM and 16GB of onboard storage. The Chromebit also includes connectivity options such as Wi-Fi 802.11ac, Bluetooth 4.0 and a USB 2.0 port.
+                  The USB 2.0 port on the dongle lets you connect peripherals such as a USB mouse or keyboard, a pen drive or an external hard disk.</p>
                 <br>
                 <div class="img"><img src="./images/articles/dongle.JPG"></div>
               </div>
           </div>
+          <br>
+          <sup>[1]</sup> : On <i>http://www.amazon.in</i> as of 24<sup>th</sup> February 2016<br>
+          Asus is a trademark of Asus Inc. and Chromebit and Chrome OS are trademarks of Google Inc.
         </div>
       </div>
 
