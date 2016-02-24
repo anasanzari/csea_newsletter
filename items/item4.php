c077bbf806be373034adbd8a378f7bbe30eaa074
Piyush Bhopalka <pbhopalka@gmail.com>
2016-02-25 00:14:16 +0530

    Update CSEA updates page

--- items/item4.php
+++ items/item4.php
@@ -3,27 +3,26 @@
   <div class="content page4">
     <div class="scroller">
       <h2>CSEA Updates</h2>
-      <h3>Activities conducted by CSEA in 2015-16</h3><br>
       <hr><br>
       <div class="container-fluid">
         <div class="row">
           <div class="col-md-6">
             <ul>
-              <div class="img"><img src="./images/cseaUpdates/paulTalk.jpg"></a></div><br>
-              <div class="img"><img src="./images/cseaUpdates/linuxWorkshop.jpg"></a></div><br>
-              <div class="img"><img src="./images/cseaUpdates/codeMaestros1.jpg"></a></div><br>
-              <div class="img"><img src="./images/cseaUpdates/codeMaestros2.jpg"></a></div><br>
+                <li>C-SCAN (Computer Science Conference of All NITs) was successfully conducted. <a href="http://cscan.org.in/">(Website)</a></li><br>
+                <li>Linux Workshop was conducted to teach basic linux commands. Presentation available <a href="http://athena.nitc.ac.in/piyush_b130057cs/Linux_Workshop/Slide/index.html#/">here</a></li><br>
+                <li>A Git workshop was conducted to teach the basics of Git, a version control system.</li><br>
+
             </ul>
           </div>
           <div class="col-md-6">
             <ul>
-              <div class="img"><img src="./images/cseaUpdates/android.jpg"></a></div><br>
-              <div class="img"><img src="./images/cseaUpdates/gitWorkshop.jpg"></a></div><br>
-              <div class="img"><img src="./images/cseaUpdates/MNNmemorialTalk.jpg"></a></div><br>
-              <div class="img"><img src="./images/cseaUpdates/intelTalk.jpg"></a></div><br>
+                <li>A 5-week Android workshop is ongoing with more than 60 registered participants. Presentation available <a href="http://athena.nitc.ac.in/piyush_b130057cs/Android_Workshop/index.html#/">here</a></li><br>
+                <li>A talk on "Extracting Performance from next gen Intel Architecture" from Austin Cherian, Business Development Manager was conducted successfully</li><br>
+                <li>A workshop on "Rust Programming Language" was conducted by <a href="http://nibrahim.net.in/">Noufal Ibrahim</a></li><br>
             </ul>
           </div>
         </div>
+        <br><h4 style="font-size: 1.25em;">For more information, visit our <a href="http://cse.nitc.ac.in/">website</a>. Also follow us on <a href="https://www.facebook.com/CSEA.NITC/">Facebook</a>!</h4>
       </div>
     </div>
   </div>
