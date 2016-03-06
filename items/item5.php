88d15a5fd43666c317b3da01f0cd59fb1ad6e63b
Aashish Satyajith <ankarathaashish@gmail.com>
2016-03-07 01:08:38 +0530

    added IIT Ropar internship details

--- items/item5.php
+++ items/item5.php
@@ -12,6 +12,7 @@
                 <li>IIIT Allahabad has opened call for its <a href="https://www.iiita.ac.in/downloads/announcements/uploads/Summer%20Research%20Internships%20Program%202016386.pdf">Summer Internship Programme 2015</a> and will accept applications till 15<sup>th</sup> April.</li>
                 <li>IIT Guwahati is hosting a <a href="https://www.iitg.ernet.in/cseweb/summerinternship/home">Summer Internship Program</a>. This one has its deadline late in March.</li>
                 <li>IIT Bhubaneswar's <a href="http://webapps.iitbbs.ac.in/internship-application/">Internship Programme for Summer</a> also has its deadline late in March.</li>
+                <li>IIT Ropar's portal for <a href="http://www.iitrpr.ac.in/summer-internship">internship applications</a> will be open till 28<sup>th</sup> March.</li>
                 <li>Also, don't miss out on some amazing learning opportunities at a few startups.</li>
               </ul>
             </div>
