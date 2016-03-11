531cf0be372d9b3b5878e0fff3b8c533c00054b3
Anas Ansari <anasanzari@gmail.com>
2016-03-11 14:08:12 +0530

    Editorial Edit

--- items/editorial.php
+++ items/editorial.php
@@ -10,13 +10,15 @@
             <div class="col-md-6">
               <p>“Sometimes it is the people who no one imagines anything of who do the things that no one can imagine.” </p>
 
-              <p>Every mathematician or a computer scientist would have dreamt of reaching great heights as high as those scaled by Mr. Alan Turing. This man, a brilliant mathematician, logistician and cryptanalyst, needs no further introduction. His contribution to the field of theoretical computer science shaped this field as we see it today. The first issue of the official newsletter of the Department of Computer Science and Engineering, NIT Calicut pays homage to him. The machine, named after its inventor, is a simple mathematical model that is used to simulate the logic of any algorithm.</p>
-              <p>On a personal note, ever since I learnt of the existence of this machine, I was in awe, of both the machine and its inventor. The Wiki page of the machine speaks of " indeed capturing the informal notion of effective methods in logic and mathematics". And then, it struck me. For me, the machine represents the black box called 'Life'. Every problem does have a solution. And even if it doesn't, that shouldn't deter us from taking the next step towards success.  </p>
-            </div>
+              <p>Every mathematician or a computer scientist would have dreamt of reaching great heights as high as those scaled by Mr. Alan Turing. This man, a brilliant mathematician, logistician and cryptanalyst, needs no further introduction. His contribution to the field of theoretical computer science shaped this field as we see it today.</p>
+              <p>The first issue of the official newsletter of the Department of Computer Science and Engineering, NIT Calicut pays homage to him. The machine, named after its inventor, is a simple yet a highly powerful mathematical model used to simulate the logic of any algorithm.</p>
+              <p>This newsletter, aptly named Threads, refers not only to its relevance in the field of Operating Systems but also to the fact that it is the little things are often the most important.</p>
+                </div>
             <div class="col-md-6">
-              <p>The purpose of this newsletter is to share knowledge, tit bits and updates from the ever-changing CS scenario. This newsletter will feature articles from our present students, alumni, updates from the Free and Open Source community apart from an informal section for our coders and hackers to take lighthearted breaks after their long hours at work.</p>
-              <p>This issue came into light only due to the creativity and toil of several students who put in their best effort. My heartfelt gratitude to Dr. Vinod Pathari sir, Faculty Editor without whom this would have not been possible. My sincere thanks to the Head of the Department, Dr. K.A Abdul Nazeer and to the CSEA faculty incharges, Dr. S.D Madhukumar and Ms. A. Lijiya for their support.  </p>
+              <p>The purpose of this newsletter is to share knowledge, tit bits and updates from the ever-changing CS scenario. This newsletter will feature articles from our current students, alumni, updates from the Free and Open Source community apart from an informal section for our coders and hackers to take lighthearted breaks after their long hours at work. This issue came into light only due to the creativity and toil of several students who put in their best effort. </p>
+              <p>My heartfelt gratitude to Dr. Vinod Pathari sir, Staff Editor without whom this would have not been possible. My sincere thanks to the Head of the Department, Dr. K.A Abdul Nazeer and to the CSEA faculty incharges, Dr. S.D Madhukumar and Ms. A. Lijiya for their support.</p>
               <p>Happy Reading!</p>
+              <p><em>Aishwarya, Student Editor, Threads</em></p>
             </div>
           </div>
         </div>
